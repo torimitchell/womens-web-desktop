529fce87b2950eb595fdbf3aeb38a943771e7b39
torimitchell <122581789+torimitchell@users.noreply.github.com>
2023-03-30 08:08:11 -0500

    Delete script directory

--- script/ww-script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-    console.log("Hi")
-});
-
-document.querySelector("#my_button").addEventListener("click", function() {
-    console.log("I am in button")
-    bd = document.querySelector(".dict_info")
-    bd.classList.toggle("font2")
-});
